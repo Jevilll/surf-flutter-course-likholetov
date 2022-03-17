6a87c85551b9cb0615d8319ce21bf80d9583dd1e
Jevilll <32613868+Jevilll@users.noreply.github.com>
2022-03-17 20:23:30 +0300

    Задание 4.2 Добавил scaffold (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(const App());
@@ -9,66 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Заголовок',
-      home: MyFirstStatefulWidget(),
+    return const MaterialApp(
+      home: SightListScreen(),
     );
   }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyFirstStatefulWidget(),
-    );
-  }
-}
-
-class MyFirstStatelessWidget extends StatelessWidget {
-
-  MyFirstStatelessWidget({Key? key}) : super(key: key);
-
-  int _counter = 0;
-
-  //Type getRuntimeType() => context.runtimeType;
-  //Error: The getter 'context' isn't defined for the class 'MyFirstStatelessWidget'.
-
-  @override
-  Widget build(BuildContext context) {
-    print('Вызов build у stateless: ${++_counter}');
-    return Container(
-      child: const Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-}
-
-class MyFirstStatefulWidget extends StatefulWidget {
-  const MyFirstStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyFirstStatefulWidget> createState() => _MyFirstStatefulWidgetState();
-}
-
-class _MyFirstStatefulWidgetState extends State<MyFirstStatefulWidget> {
-  int _counter = 0;
-  @override
-  Widget build(BuildContext context) {
-    print('Вызов build у statefull: ${++_counter}');
-    return Container(
-      child: const Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-
-  Type getRuntimeType() => context.runtimeType;
 }
