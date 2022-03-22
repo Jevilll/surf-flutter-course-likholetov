8a9c6ac4c478bb6a6c6b1fbf1dfcbd121f2ec6cf
Jevilll <32613868+Jevilll@users.noreply.github.com>
2022-03-22 12:47:08 +0300

    4 4 text (#3)
    * Задание 4.4 Добавил appbar
    
    * Задание 4.4 Добавил appbar c цветными буквами

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Экран списка достопримечательностей.
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
 
@@ -10,9 +11,30 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TextField(),
+    return Scaffold(
+      appBar: AppBar(
+        title: RichText(
+          text: const TextSpan(
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              color: Color(0xFF252849),
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+            ),
+            children: [
+              TextSpan(text: 'С', style: TextStyle(color: Colors.green),),
+              TextSpan(text: 'писок\n'),
+              TextSpan(text: 'и', style: TextStyle(color: Colors.yellow),),
+              TextSpan(text: 'нтересных мест'),
+            ],
+          ),
+          textAlign: TextAlign.left,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 100,
       ),
     );
   }
