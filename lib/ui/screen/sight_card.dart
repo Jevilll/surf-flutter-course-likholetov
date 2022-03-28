9719d32b113f39062b6ec8b7de24f9d82ad056b2
jevil <a.liholetoff@yandex.ru>
2022-03-28 13:33:04 +0300

    Задание 4.6.4 Добавил SizedBox, AspectRatio и PreferredSizeWidget

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -12,65 +12,71 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(15),
-      child: Column(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 96,
-            width: double.infinity,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Positioned(
-                  left: 16,
-                  top: 16,
-                  child: Text(
-                    sight.type.name,
-                    style: AppTextStyles.smallBold,
-                  ),
-                ),
-                const Positioned(
-                  top: 19,
-                  right: 18,
-                  child: Icon(Icons.heart_broken_outlined),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            color: AppColors.background,
-            height: 92,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
+    return AspectRatio(
+      aspectRatio: 3 / 2,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 96,
+              width: double.infinity,
+              child: Stack(
+                fit: StackFit.expand,
                 children: [
-                  SizedBox(
-                    width: double.infinity,
+                  Positioned(
+                    left: 16,
+                    top: 16,
                     child: Text(
-                      sight.shortDescription,
-                      maxLines: 2,
-                      style: AppTextStyles.text,
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.ellipsis,
+                      sight.type.name,
+                      style: AppTextStyles.smallBold,
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 2),
-                    width: double.infinity,
-                    child: const Text(
-                      AppStrings.shortDescription,
-                      textAlign: TextAlign.left,
-                      style: AppTextStyles.small,
-                    ),
+                  const Positioned(
+                    top: 19,
+                    right: 18,
+                    child: Icon(Icons.heart_broken_outlined),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+            // todo академическая вставка, убрать в дальнейшем
+            const SizedBox(width: double.infinity, height: 16,),
+            Expanded(
+              child: Container(
+                color: AppColors.background,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          sight.shortDescription,
+                          maxLines: 2,
+                          style: AppTextStyles.text,
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 2),
+                        width: double.infinity,
+                        child: const Text(
+                          AppStrings.shortDescription,
+                          textAlign: TextAlign.left,
+                          style: AppTextStyles.small,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
