45e946d792f4349dfc0a86c619f1d2a6625036b9
jevil <a.liholetoff@yandex.ru>
2022-05-04 12:46:51 +0300

    Добавил прокрутку галереи детализации места на сливерах

--- lib/ui/screen/sight_details_screen.dart
+++ lib/ui/screen/sight_details_screen.dart
@@ -45,141 +45,104 @@ class _SightDetailsState extends State<SightDetails> {
     final theme = Theme.of(context);
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Material(
-              child: Stack(
-                children: [
-                  SizedBox(
-                    height: 360,
-                    width: double.infinity,
-                    child: PageView.builder(
-                      itemCount: widget.sight.images.length,
-                      controller: _pageController,
-                      onPageChanged: (page) {},
-                      itemBuilder: (context, position) {
-                        return ImagePreview(
-                          imgUrl: widget.sight.images[position],
-                        );
-                      },
-                    ),
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverPersistentHeader(
+            delegate: _GalleryDelegate(
+              photos: widget.sight.images,
+              controller: _pageController,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+                  width: double.infinity,
+                  child: Text(
+                    widget.sight.name,
+                    style: theme.textTheme.titleMedium,
                   ),
-                  if (widget.sight.images.length > 1)
-                    Positioned.fill(
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: _PageIndicator(
-                          controller: _pageController,
-                          pageCount: widget.sight.images.length,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16, top: 2, right: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        widget.sight.type.name,
+                        style: AppTextStyles.smallBold.copyWith(
+                          color: theme.colorScheme.secondary2,
                         ),
                       ),
-                    ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, top: 36),
-                    child: Container(
-                      height: 32,
-                      width: 32,
-                      decoration: const BoxDecoration(
-                        color: AppColors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      const SizedBox(
+                        width: 16,
                       ),
-                      child: ButtonSvgIcon(
-                        icon: AppIcons.arrow,
-                        color: Theme.of(context).colorScheme.main,
+                      Text(
+                        widget.sight.workingHours,
+                        style: AppTextStyles.small.copyWith(
+                          color: theme.colorScheme.inactiveBlack,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16, top: 24, right: 16),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      widget.sight.details,
+                      style: theme.textTheme.bodySmall,
+                    ),
+                  ),
+                ),
+                ButtonRounded(
+                  title: AppStrings.buildARoute,
+                  onPressed: () {
+                    logger.i('Нажатие на кнопку построить маршрут');
+                  },
+                  icon: AppIcons.go,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 16, top: 24, right: 16),
+                  height: 1,
+                  width: double.infinity,
+                  color: AppColors.inactiveBlack,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 16, top: 8, right: 16),
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ButtonWithoutBorders(
+                        title: AppStrings.toSchedule,
+                        icon: AppIcons.calendar,
+                        height: 40,
+                        width: 164,
+                        color: Theme.of(context).colorScheme.text,
+                        onPressed: null,
+                      ),
+                      ButtonWithoutBorders(
+                        title: AppStrings.toFavorites,
+                        icon: AppIcons.heart,
+                        height: 40,
+                        width: 164,
+                        color: Theme.of(context).colorScheme.text,
                         onPressed: () {
-                          Navigator.pop(context);
+                          logger.i('Нажатие на кнопку в избранное');
                         },
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 16, top: 24, right: 16),
-              width: double.infinity,
-              child: Text(
-                widget.sight.name,
-                style: theme.textTheme.titleMedium,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 16, top: 2, right: 16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                textBaseline: TextBaseline.alphabetic,
-                children: [
-                  Text(
-                    widget.sight.type.name,
-                    style: AppTextStyles.smallBold.copyWith(
-                      color: theme.colorScheme.secondary2,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Text(
-                    widget.sight.workingHours,
-                    style: AppTextStyles.small.copyWith(
-                      color: theme.colorScheme.inactiveBlack,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 16, top: 24, right: 16),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  widget.sight.details,
-                  style: theme.textTheme.bodySmall,
                 ),
-              ),
+              ],
             ),
-            ButtonRounded(
-              title: AppStrings.buildARoute,
-              onPressed: () {
-                logger.i('Нажатие на кнопку построить маршрут');
-              },
-              icon: AppIcons.go,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 16, top: 24, right: 16),
-              height: 1,
-              width: double.infinity,
-              color: AppColors.inactiveBlack,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 16, top: 8, right: 16),
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ButtonWithoutBorders(
-                    title: AppStrings.toSchedule,
-                    icon: AppIcons.calendar,
-                    height: 40,
-                    width: 164,
-                    color: Theme.of(context).colorScheme.text,
-                    onPressed: null,
-                  ),
-                  ButtonWithoutBorders(
-                    title: AppStrings.toFavorites,
-                    icon: AppIcons.heart,
-                    height: 40,
-                    width: 164,
-                    color: Theme.of(context).colorScheme.text,
-                    onPressed: () {
-                      logger.i('Нажатие на кнопку в избранное');
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -217,5 +180,79 @@ class _PageIndicator extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+/// Делегат сворачиваемого сливера галереи.
+class _GalleryDelegate extends SliverPersistentHeaderDelegate {
+  final List<String> photos;
+  final PageController controller;
+
+  @override
+  double get maxExtent => 360;
+
+  @override
+  double get minExtent => 0;
+
+  _GalleryDelegate({
+    required this.photos,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Material(
+      child: Stack(
+        children: [
+          PageView.builder(
+            itemCount: photos.length,
+            controller: controller,
+            itemBuilder: (context, position) {
+              return ImagePreview(
+                imgUrl: photos[position],
+              );
+            },
+          ),
+          if (photos.length > 1)
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: _PageIndicator(
+                  controller: controller,
+                  pageCount: photos.length,
+                ),
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 36),
+            child: Container(
+              height: 32,
+              width: 32,
+              decoration: const BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: ButtonSvgIcon(
+                icon: AppIcons.arrow,
+                color: Theme.of(context).colorScheme.main,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return oldDelegate is! _GalleryDelegate ||
+        minExtent != oldDelegate.minExtent ||
+        maxExtent != oldDelegate.maxExtent ||
+        snapConfiguration != oldDelegate.snapConfiguration ||
+        photos != oldDelegate.photos ||
+        controller != oldDelegate.controller;
   }
 }
