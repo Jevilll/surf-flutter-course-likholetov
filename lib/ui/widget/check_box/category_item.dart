eda34a310facb3f85826c6ab2933f2c7790ac63b
jevil <a.liholetoff@yandex.ru>
2022-05-11 18:32:20 +0300

    9-2 адаптировал категории под размер экрана

--- lib/ui/widget/check_box/category_item.dart
+++ lib/ui/widget/check_box/category_item.dart
@@ -10,48 +10,56 @@ class CategoryItem extends StatelessWidget {
   final bool isSelected;
   final VoidCallback onTap;
 
-  const CategoryItem(this.category, {required this.isSelected, required this.onTap, Key? key})
-      : super(key: key);
+  const CategoryItem(
+    this.category, {
+    required this.isSelected,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Column(
-      children: [
-        Stack(
-          children: [
-            InkWell(
-              customBorder: const CircleBorder(),
-              onTap: onTap,
-              child: Container(
-                height: 64,
-                width: 64,
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.green.withOpacity(0.16),
-                  shape: BoxShape.circle,
-                ),
-                child: UnconstrainedBox(
-                  child: SvgPicture.asset(
-                    category.icon,
-                    height: 32,
-                    width: 32,
-                    color: theme.colorScheme.green,
+    return SizedBox(
+      height: 96,
+      width: 92,
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: onTap,
+                child: Container(
+                  height: 64,
+                  width: 64,
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.green.withOpacity(0.16),
+                    shape: BoxShape.circle,
+                  ),
+                  child: UnconstrainedBox(
+                    child: SvgPicture.asset(
+                      category.icon,
+                      height: 32,
+                      width: 32,
+                      color: theme.colorScheme.green,
+                    ),
                   ),
                 ),
               ),
-            ),
-            if (isSelected) _CheckItem(),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 12),
-          child: Text(
-            category.name,
-            style: theme.textTheme.bodySmall,
+              if (isSelected) _CheckItem(),
+            ],
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: Text(
+              category.name,
+              style: theme.textTheme.bodySmall,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
