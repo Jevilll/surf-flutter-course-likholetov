f05051e64c5a0f4ddff59ecadbe558071a324175
Jevilll <32613868+Jevilll@users.noreply.github.com>
2022-06-05 12:14:19 +0300

    Подключил зависимости интеракторов через MultiProvider (#22)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:places/data/repository/location_repository_impl.dart';
+import 'package:places/data/repository/places_repository_impl.dart';
+import 'package:places/domain/interactor/places_interactor.dart';
+import 'package:places/domain/interactor/search_interactor.dart';
+import 'package:places/domain/interactor/settings_interactor.dart';
+import 'package:places/domain/repository/location_repository.dart';
+import 'package:places/domain/repository/places_repository.dart';
 import 'package:places/res/app_themes.dart';
 import 'package:places/ui/screen/splash_screen.dart';
 import 'package:places/utils/common.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const App());
@@ -28,17 +36,40 @@ class _AppState extends State<App> {
       });
     });
 
-    return MaterialApp(
-      home: const SplashScreen(),
-      theme: theme,
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        Provider<PlacesRepository>(create: (context) => PlacesRepositoryImpl()),
+        Provider<LocationRepository>(
+          create: (context) => LocationRepositoryImpl(),
+        ),
+        Provider<PlacesInteractor>(
+          create: (context) => PlacesInteractor(
+            context.read<PlacesRepository>(),
+            context.read<LocationRepository>(),
+          ),
+          dispose: (context, interactor) {
+            interactor.dispose();
+          },
+        ),
+        Provider<SearchInteractor>(
+          create: (context) => SearchInteractor(
+            context.read<PlacesRepository>(),
+          ),
+        ),
+        Provider<SettingsInteractor>(create: (context) => SettingsInteractor()),
       ],
-      supportedLocales: const [
-        Locale('ru'),
-      ],
+      child: MaterialApp(
+        home: const SplashScreen(),
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('ru'),
+        ],
+      ),
     );
   }
 }
